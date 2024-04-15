610234bfa67e513f235a74f5be8c8204df414a36
Victor <victor.lopez@king.com>
2024-04-16 00:45:24 +0200

    Better calculation

--- Source/PracaInz/PracaInzGameState.cpp
+++ Source/PracaInz/PracaInzGameState.cpp
@@ -417,12 +417,7 @@ void APracaInzGameState::SpawnPlanetFromJsonData(const FPlanetData& PlanetData)
 		NewPlanet->Diameter = Diameter;
 		NewPlanet->Name = PlanetData.Name;
 		NewPlanet->SetActorScale3D(FVector(0.1)); // Assuming a uniform scale
-		FMath::RandRange(0.0f, (float)M_PI * 2.0f);
-
-		NewPlanet->InitialVelocity.Y = 29.780001;
-		
-		NewPlanet->InitialSetup();
-
+		NewPlanet->Inclination = 0.0039;
 		NewPlanet->OrbitColor = FColor(255, 255, 255, 255); // Setting a default color, adjust as needed
 	}
 }
