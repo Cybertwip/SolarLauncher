7e10f7d04ac175d5ccebf001e08013bcf33a405c
Victor <victor.lopez@king.com>
2024-04-16 17:43:30 +0200

    Thruster finish off

--- Source/PracaInz/Rocket.cpp
+++ Source/PracaInz/Rocket.cpp
@@ -259,8 +259,8 @@ void ARocket::AdjustThrustAsNearingTarget()
 	float Distance = FVector::Dist(GetActorLocation(), TargetLocation);
 	
 	// Define the distance range within which to start reducing thrust
-	float StartReduceDistance = 3500.0f; // Start reducing thrust at this distance
-	float MinReduceDistance = 200.0f;    // Minimum distance where thrust should be at its minimum
+	float StartReduceDistance = 500.0f; // Start reducing thrust at this distance
+	float MinReduceDistance = 100.0f;    // Minimum distance where thrust should be at its minimum
 	
 	if (Distance < StartReduceDistance)
 	{
